6b1e515613c034bb09ddbade706c480fb1cc54ba
dn898322-lgtm <dn898322@gmail.com>
2026-01-20 08:16:15 +0700

    Implement electricity bill calculation in bai3.c
    Updated personal information and implemented electricity bill calculation logic.

--- bai3.c
+++ bai3.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [nguyễn Tấn Dương]
+ * MSSV:      [PS47990]
+ * Lớp:       [com108-cs21301]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH TÍNH TIỀN ĐIỆN 
@@ -13,11 +13,25 @@
 int main(){
     
     // Khai báo biến
+    int so_dien;
+    float tien_dien;
 
 
     // Nhập dữ liệu
-
-
+    printf("Nhap so dien tieu thu: ");
+    scanf("%d", &so_dien);
     // Xử lý, tính toán VÀ Hiển thị kết quả
 
+    if (so_dien <= 50) {
+        tien_dien = so_dien * 1000;
+    } else if (so_dien <= 100) {
+        tien_dien = 50 * 1000 + (so_dien - 50) * 1200;
+    } else if (so_dien <= 200) {
+        tien_dien = 50 * 1000 + 50 * 1200 + (so_dien - 100) * 1500;
+    } else {
+        tien_dien = 50 * 1000 + 50 * 1200 + 100 * 1500 + (so_dien - 200) * 250;
+    }
+
+    printf("So tien dien phai tra la: %.2f\n", tien_dien);
+    return 0;
 }
